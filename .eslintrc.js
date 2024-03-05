a433bd3d894edf11e5c6308e0559bde34bf9bbd8
Mohammed Yasin Mulla <yasinspy1999@gmail.com>
2024-03-05 15:32:37 +0530

    chore: add eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
-  extends: ["universe/native"],
+  extends: ['universe/native'],
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+    'import/order': 'off',
+    endOfLine: 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+      },
+    ],
+  },
 };
